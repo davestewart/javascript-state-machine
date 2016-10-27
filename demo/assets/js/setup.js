e36adcc74ccf96f5b82bfa16eb0d41bd27eadc5a
Dave Stewart <dave@davestewart.co.uk>
2016-10-28 00:10:33 +0100

    Big demo update
    Moved Config examples second in index
    Converted all object configuration to shorthand notation
    Added setup example
    Added object configuration example
    Improved shorthand notation examples
    Added basic handlers example
    Added VueRouter example

--- demo/assets/js/setup.js
+++ demo/assets/js/setup.js
@@ -1,57 +1,65 @@
-if (window.StateMachine) {
-    window.StateMachine = StateMachine.default;
-}
+// ------------------------------------------------------------------------------------------------
+// utility functions
 
-$(function(){
-    $('body').append('<a id="home" href="/">Home</a>');
-});
-
-var fsm, $states, $buttons;
-
-function update() {
-
-    // assign state and paused
-    $states
-        .attr('data-state', fsm.state)
-        .toggleClass('paused', fsm.isPaused());
-
-    // assign active class to the current state
-    $states
-        .find('article')
-        .removeClass('active')
-        .filter('#'  + fsm.state)
-        .addClass('active');
-
-    // update buttons
-    $buttons
-        .each(function(i, e){
-            e.disabled = ! fsm.can(e.name) || fsm.isPaused();
-        });
-}
-
-function setup(fsm) {
-
-    // assign to window
-    window.fsm = fsm;
-
-    // elements
-    $states = $('#states');
-    $buttons = $('#controls button');
-
-    // bind button clicks to fsm actions
-    $buttons.on('click', function (event) {
-        fsm.do(event.target.name);
+    $(function(){
+        $('body').append('<a id="home" href="/">Home</a>');
     });
 
-    // update UI when fsm state changes
-    fsm.on('update', update);
+    function show(config)
+    {
+        var text = 'transitions: [\n    \'' + config.transitions.join('\',\n    \'') + '\'\n]';
+        $('pre:first-of-type').text(text);
+    }
 
-    // update
-    update();
-    console.log(fsm)
-}
 
-function show(config)
-{
-    $('pre:first-of-type').text(config.transitions.join('\n'));
-}
+// ------------------------------------------------------------------------------------------------
+// state machine setup
+
+    function update() {
+
+        // assign state and paused
+        $states
+            .attr('data-state', fsm.state)
+            .toggleClass('paused', fsm.isPaused());
+
+        // assign active class to the current state
+        $states
+            .find('article')
+            .removeClass('active')
+            .filter('#'  + fsm.state)
+            .addClass('active');
+
+        // update buttons
+        $buttons
+            .each(function(i, e){
+                e.disabled = ! fsm.can(e.name) || fsm.isPaused();
+            });
+    }
+
+    function setup(fsm) {
+
+        // assign to window
+        window.fsm = fsm;
+
+        // elements
+        $states = $('#states');
+        $buttons = $('#controls button');
+
+        // bind button clicks to fsm actions
+        $buttons.on('click', function (event) {
+            fsm.do(event.target.name);
+        });
+
+        // update UI when fsm state changes
+        fsm.on('update', update);
+
+        // update
+        update();
+        console.log(fsm)
+    }
+
+    if (window.StateMachine) {
+        window.StateMachine = StateMachine.default;
+    }
+
+    var fsm, $states, $buttons;
