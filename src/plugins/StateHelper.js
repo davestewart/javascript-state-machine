6e16560e733c25a82be0d311f68dfb042295e4a3
Dave Stewart <dave@davestewart.co.uk>
2016-11-06 21:19:36 +0000

    Updated StateHelper class
    Separated out pause and state change events
    Added support for adding and removing of states

--- src/plugins/StateHelper.js
+++ src/plugins/StateHelper.js
@@ -2,9 +2,17 @@ import { toHash } from '../utils/utils';
 
 export default function StateHelper (fsm)
 {
+    var onPause = this.onPause.bind(this);
+    var onModify = this.onModify.bind(this);
     this.data = {};
     this.fsm = fsm;
-    this.fsm.on('update', this.update.bind(this));
+    this.fsm
+        .on('change', this.onChange.bind(this))
+        .on('pause', onPause)
+        .on('resume', onPause)
+        .on('add', onModify)
+        .on('remove', onModify);
+    this.reset();
     this.update();
 }
 
@@ -12,20 +20,48 @@ StateHelper.prototype =
 {
     fsm : null,
 
-    data :
-    {
-        name     : '',
-        paused   : false,
-        is       : {},
-        actions  : {},
-        states   : {}
-    },
+    data : null,
 
     update:function()
     {
+        this.onChange();
+        this.onPause();
+        this.onModify();
+    },
+
+    reset: function ()
+    {
+        this.data =
+        {
+            name     : '',
+            index    : -1,
+            paused   : false,
+            is       : {},
+            actions  : {},
+            states   : {},
+            all:{
+                states  :[],
+                actions :[]
+            }
+        }
+    },
+
+    onPause:function(event)
+    {
+        this.data.paused        = this.fsm.isPaused();
+    },
+
+    onModify:function(event)
+    {
+        this.data.all.states    = this.fsm.transitions.getStates();
+        this.data.all.actions   = this.fsm.transitions.getActions();
+    },
+
+    onChange:function(event)
+    {
         var fsm                 = this.fsm;
         this.data.name          = fsm.state;
-        this.data.paused        = fsm.isPaused();
+        this.data.index         = this.fsm.transitions.states.indexOf(this.data.name);
         this.data.states        = toHash(fsm.transitions.getToStates(fsm.state));
         this.data.actions       = toHash(fsm.transitions.getActionsFrom(fsm.state));
         this.data.is            = {};
