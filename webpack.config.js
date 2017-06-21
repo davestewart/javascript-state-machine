224865b9d1c07da9de5845b667083aa571f14802
Dave Stewart <dave@davestewart.co.uk>
2017-06-21 20:07:25 +0100

    Updated to Webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -8,52 +8,56 @@ var libraryName = 'StateMachine';
 var fileName = 'state-machine';
 var outputFile;
 var plugins = [
-  //new webpack.optimize.CommonsChunkPlugin("components", "components.min.js")
+    //new webpack.optimize.CommonsChunkPlugin("components", "components.min.js")
 ];
 
 if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = '[name].min.js';
+    plugins.push(new UglifyJsPlugin({minimize: true}));
+    outputFile = '[name].min.js';
 } else {
-  outputFile = '[name].js';
+    outputFile = '[name].js';
+}
+
+function src (file) {
+    return __dirname + '/src/' + file;
 }
 
 var config = {
-  entry: {
-    'StateMachine': [__dirname + '/src/StateMachine.js'],
-    'StateHelper': [__dirname + '/src/helpers/index.js']
-  },
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/dist',
-    //umdNamedDefine: true,
-    //libraryTarget: 'var',
-    libraryTarget: 'umd',
-    filename: outputFile,
-    library: '[name]'
-  },
-  module: {
-    loaders: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel',
-        exclude: /(node_modules|bower_components)/,
-        query: {
-          presets: ['es2015']
-        }
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: "eslint-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    root: path.resolve('./src'),
-    extensions: ['', '.js']
-  },
-  plugins: plugins
+    entry: {
+        'main': src('main.js')
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "state-machine.js",
+        libraryTarget: "umd"
+    },
+    devtool: 'source-map',
+    plugins: plugins,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: "eslint-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            path.resolve('./src'),
+            "node_modules"
+        ]
+    }
 };
 
 module.exports = config;
