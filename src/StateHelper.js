017ef68f43902f59d0ea9e43c9a20d88fd445ba6
Dave Stewart <dave@davestewart.co.uk>
2017-06-21 20:09:55 +0100

    Moved main files around

---
+++ src/StateHelper.js
@@ -0,0 +1,9 @@
+import object from './helpers/ObjectHelper';
+import jQuery from'./helpers/jQueryHelper';
+import vueRouter from'./helpers/VueRouter';
+
+export default {
+    object,
+    jQuery,
+    vueRouter
+};
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import StateMachine from './StateMachine'
+import StateHelper from './StateHelper'
+
+export default StateMachine
+
+export {
+    StateMachine,
+    StateHelper
+}
