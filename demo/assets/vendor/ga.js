49f1a4711b3f8ae8864273dd5030f228ec8cdb89
Dave Stewart <dave@davestewart.co.uk>
2018-07-08 19:46:30 +0100

    Fixed analytics function

--- demo/assets/vendor/ga.js
+++ demo/assets/vendor/ga.js
@@ -1,11 +1,16 @@
-(function () {
-  function track (id) {
-    document.write('<script id="ga" async src="https://www.googletagmanager.com/gtag/js?id=' + id + '"></script>\n')
-    window.dataLayer = [
-      ['js', new Date()],
-      ['config', id]
-    ]
-  }
+(function track (id) {
+  // add data
+  window.dataLayer = window.dataLayer || []
+  function gtag () { dataLayer.push(arguments) }
+  gtag('js', new Date())
+  gtag('config', 'UA-603607-17')
 
-  track('UA-603607-17')
-}())
+  // insert tag
+  var ga = document.createElement('script')
+  ga.src = 'https://www.googletagmanager.com/gtag/js?id=' + id
+  ga.type = 'text/javascript'
+  ga.async = true
+  var script = document.getElementsByTagName('script')[0]
+  script.parentNode.insertBefore(ga, script)
+
+}('UA-603607-17'))
